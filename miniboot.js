8ff7d66754af09cba61f254694a39d6eada8e0d3
FabioVergani <fabio.vergani.1975@gmail.com>
2015-01-06 17:14:21 +0100

    Update miniboot.js

--- miniboot.js
+++ miniboot.js
@@ -12,7 +12,7 @@ console.clear();
  function once(e,s,l){function f(o){logEvent(o);l(o);o.target.removeEventListener(o.type,f);}on(e,s,f);}
 
  var d=wd, w=d.defaultView, dr=d.documentElement, dp=d.body,
- f=dummy, dir=f, log=f, logs=f, report=f, logEvent=f,
+ f=dummy, dir=f, log=f, logs=f, dirs=f, report=f, logEvent=f, logHR=f,
  sb=function(o,p){return o[p].bind(o);},
  $id=sb(d,'getElementById');
 
@@ -27,6 +27,9 @@ console.clear();
 
 	dir=c('dir');
 	log=c('log');
+
+	logHR=function(s){log((s?s+'\n':'')+'%c\n','border:0;font:0/0 sans;height:0;padding:0 100% 0 0;border-bottom:1px dashed #888;');};
+	dirs=function(){each(arguments,dir);logHR();};
 
 	logEvent=function(o){
 	 function _Unix2Hrt(t){return new Date(t*1000).toTimeString().substring(0,8);}
@@ -52,7 +55,7 @@ console.clear();
 	 groupEnd();
 	}
 
-	report=function(o){
+	report=function(){
 	 groupCollapsed('window');log('(w)%O',w);groupEnd();
 	 logs(
 		['document (d)',d],
@@ -66,21 +69,17 @@ console.clear();
  c=sb=f;//null
 
 
+ function SetupTree(treeid){
+	var tree=$id(treeid),root=tree.firstElementChild;
+	dirs(tree,root);
 
 
+ };
 
 
-function init(o){
-log('ready');
-};
-function finalize(o){
-log('complete');
-report();
-};
-
-
-
-
+ //
+ function init(){logHR('ready');SetupTree('mylist');};
+ function finalize(){logHR('complete');report();};
  //
  log('%cGMT+1(W. Europe Standard Time)','color:#ddd');
  once(d,'DOMContentLoaded',init);
